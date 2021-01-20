3aee01fe72c04c8de665b8be3c0a00d6ef9b2868
문진석 <m_jinseok@naver.com>
2021-01-20 21:30:16 +0900

    리사이즈시, 토글 버튼 숨기기 및 펼친 메뉴 닫기
    - resize eventListener

--- main.js
+++ main.js
@@ -4,10 +4,24 @@
   // convention
   const $toggles = document.querySelectorAll(".toggle"); // return NodeList
   const $toggleBtn = document.getElementById("toggle-btn");
+
   $toggleBtn.addEventListener("click", () => toggleElements());
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 1024) {
+      offElements();
+    }
+  });
+
   const toggleElements = () => {
     [].forEach.call($toggles, (toggle) => {
       toggle.classList.toggle("on");
+    });
+  };
+
+  const offElements = () => {
+    [].forEach.call($toggles, (toggle) => {
+      toggle.classList.remove("on");
     });
   };
 })(window, document);
